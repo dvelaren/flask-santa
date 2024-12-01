d72358da21592249fb240b4dea3e9cd6f6ea6404
David Velasquez <dvelas25@eafit.edu.co>
2024-12-01 17:17:42 -0500

    Implement sample fullstack app

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")  # GET Method
-def hello_world():
+def home():
     """
     Handles the root URL of the application.
 
     Returns:
         str: A simple HTML string with a greeting message.
     """
-    return "<p>Hello, World!</p>"
+    return render_template("home.html", my_name="David")
+
+
+@app.route("/about")  # GET Method
+def about():
+    """
+    Handles the root URL of the application.
+
+    Returns:
+        str: A simple HTML string with a greeting message.
+    """
+    return render_template(
+        "about.html",
+        about_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec pur",
+    )
 
 
 if __name__ == "__main__":
